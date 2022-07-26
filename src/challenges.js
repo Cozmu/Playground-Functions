8d8ebb33e17aab71b0a48856a93ebb699a9249f7
Cozmu <www.jorgereis@gmail.com>
2022-07-26 15:32:30 -0300

    Função compareTrue adicionada

--- src/challenges.js
+++ src/challenges.js
@@ -1,44 +1,53 @@
-// Desafio 1
-function compareTrue() {
-  // Iniciando o projeto
+// Desafio 1 ----------------------------------------------------------------------------------------------------------------------------
+function compareTrue(macaco, banana) {
+  if (macaco && banana === true) {
+    return true;
+  } else if (macaco && banana === false) {
+    return false;
+  } else {
+    return false;
+  }
+}
+console.log(compareTrue(true, true));
+console.log(compareTrue(false, true));
+console.log(compareTrue(false, false));
+
+// Desafio 2 ----------------------------------------------------------------------------------------------------------------------------
+function calcArea(base, altura) {
+  let calculo
 }
 
-// Desafio 2
-function calcArea() {
-  // seu código aqui
-}
-
-// Desafio 3
+// Desafio 3 ----------------------------------------------------------------------------------------------------------------------------
 function splitSentence() {
   // seu código aqui
 }
 
-// Desafio 4
+// Desafio 4 ----------------------------------------------------------------------------------------------------------------------------
 function concatName() {
   // seu código aqui
 }
 
-// Desafio 5
+// Desafio 5 ----------------------------------------------------------------------------------------------------------------------------
 function footballPoints() {
   // seu código aqui
 }
 
-// Desafio 6
+// Desafio 6 ----------------------------------------------------------------------------------------------------------------------------
 function highestCount() {
   // seu código aqui
 }
 
-// Desafio 7
+// Desafio 7 ----------------------------------------------------------------------------------------------------------------------------
 function catAndMouse() {
   // seu código aqui
 }
 
-// Desafio 8
+// Desafio 8 ----------------------------------------------------------------------------------------------------------------------------
 function fizzBuzz() {
   // seu código aqui
 }
 
-// Desafio 9
+// Desafio 9 ----------------------------------------------------------------------------------------------------------------------------
 function encode() {
   // seu código aqui
 }
@@ -46,7 +55,7 @@ function decode() {
   // seu código aqui
 }
 
-// Desafio 10
+// Desafio 10 ----------------------------------------------------------------------------------------------------------------------------
 function techList() {
   // seu código aqui
 }
